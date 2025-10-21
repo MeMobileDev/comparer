e3fb2fb0324919c15d338a10751cc169d9e8007a
Sarankumar Venkatachalam <sarankumarvenkatachalam@Sarankumars-MacBook-Air.local>
2025-10-21 11:29:43 +0530

    Initial commit for new Flutter project

---
+++ lib/ff.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MyD extends StatelessWidget {
+  const MyD({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
